ad26c7d2d87b1f7555a268acf1f98739bf4d1289
Raditz Farhan <raditzfarhan@gmail.com>
2026-04-09 12:46:27 +0800

    docs: add PHPDoc @method annotations for IDE autocomplete

--- src/ApiResponse.php
+++ src/ApiResponse.php
@@ -8,6 +8,36 @@ use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\Response;
 
 
+/**
+ * @method static static httpCode(int $code)
+ * @method static static message(string $message)
+ * @method static static data(mixed $data)
+ * @method static static errors(array $errors)
+ * @method static static meta(array $meta)
+ * @method static static links(array $links)
+ * @method static static code(int|string $code)
+ * @method static static headers(array $headers)
+ * @method static \Illuminate\Http\JsonResponse success()
+ * @method static \Illuminate\Http\JsonResponse failed()
+ * @method static \Illuminate\Http\JsonResponse created(mixed $data = null)
+ * @method static \Illuminate\Http\JsonResponse collection(mixed $data)
+ * @method static \Illuminate\Http\JsonResponse badRequest(?string $message = null)
+ * @method static \Illuminate\Http\JsonResponse unauthorized(?string $message = null)
+ * @method static \Illuminate\Http\JsonResponse forbidden(?string $message = null)
+ * @method static \Illuminate\Http\JsonResponse notFound(?string $message = null)
+ * @method static \Illuminate\Http\JsonResponse methodNotAllowed(?string $message = null)
+ * @method static \Illuminate\Http\JsonResponse notAcceptable(?string $message = null)
+ * @method static \Illuminate\Http\JsonResponse requestTimeout(?string $message = null)
+ * @method static \Illuminate\Http\JsonResponse conflict(?string $message = null)
+ * @method static \Illuminate\Http\JsonResponse gone(?string $message = null)
+ * @method static \Illuminate\Http\JsonResponse validationError(?array $errors = null)
+ * @method static \Illuminate\Http\JsonResponse tooManyRequests(?string $message = null)
+ * @method static \Illuminate\Http\JsonResponse internalServerError(?string $message = null)
+ * @method static \Illuminate\Http\JsonResponse notImplemented(?string $message = null)
+ * @method static \Illuminate\Http\JsonResponse badGateway(?string $message = null)
+ * @method static \Illuminate\Http\JsonResponse serviceUnavailable(?string $message = null)
+ * @method static \Illuminate\Http\JsonResponse gatewayTimeout(?string $message = null)
+ */
 class ApiResponse
 {
     /**
